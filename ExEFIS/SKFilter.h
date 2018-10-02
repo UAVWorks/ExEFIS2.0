99137e3a3becf65690fa89f90830bb30a07cd064
mwavioni <git@mw-avionics.com>
2018-10-02 12:42:18 -0600

    SK FIlter is working ok

---
+++ ExEFIS/SKFilter.h
@@ -0,0 +1,43 @@
+#pragma once
+#include "Vector.h"
+
+
+class SKFilter
+{
+public:
+	SKFilter();
+	~SKFilter();
+	void setInitializationDuration(int duration);
+	bool update(float gx, float gy, float gz, float ax, float ay, float az, float hx, float hy, float hz);
+	float getRoll_rad();
+	float getPitch_rad();
+	float getYaw_rad();
+	float getHeading_rad();
+	int quad = 0;
+	imu::Vector<3> getEuler(void);
+private:
+	bool _initialized = false;
+	// timing
+	float _tnow, _tprev, _dt;
+	
+	float magHeading;
+	
+	int magbufferindex = 0;
+	
+	
+	
+	// store previous sensor values to see when updated
+	float gx_, gy_, gz_;
+	float ax_, ay_, az_;
+	float mx_, my_, mz_;
+	
+	bool gyroUpdated_, accelUpdated_, magUpdated_;
+	
+	float constrainAngle180(float dta);
+	float constrainAngle360(float dta);
+	
+	imu::Vector<3> _Euler;
+	imu::Vector<3> gyroPrev;
+	imu::Vector<3> _InitialEuler;
+};
+
